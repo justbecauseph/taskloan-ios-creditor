018d90e338f702b49a032030cde3642a64f2dd57
Mark Alain D. Rufino <mark.rufino.io@gmail.com>
2018-11-18 10:53:16 +0800

    Update ApiError.swift

--- decode-tomorrow/app/errors/ApiError.swift
+++ decode-tomorrow/app/errors/ApiError.swift
@@ -13,10 +13,11 @@ struct ApiError: Decodable {
     let message: String
 }
 
+// The format of the extension below is very important because the error will only be identified generically unless it's applied.
+
 extension ApiError: Swift.Error, LocalizedError {
 //    var localizedDescription: String { return messages.joined() }
 //    var localizedDescription: String { return message }
-    
     var errorDescription: String? {
         return message
     }
